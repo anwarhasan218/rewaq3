7be9bd93074bebc2c63cc83277f8ec5b30c12057
anwarhasan218 <40578832+anwarhasan218@users.noreply.github.com>
2025-04-24 17:07:53 +0200

    Delete add-seat-number-column.sql

--- add-seat-number-column.sql
+++
@@ -1,5 +0,0 @@
--- إضافة عمود رقم الجلوس إلى جدول الطلاب
-ALTER TABLE students ADD COLUMN seat_number VARCHAR(50);
-
--- إضافة تعليق وصفي للعمود
-COMMENT ON COLUMN students.seat_number IS 'رقم الجلوس للطالب';
